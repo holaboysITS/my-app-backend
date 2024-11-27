3d22e6191ad22514b34941dbb1b324f09b84f613
Utente1\Utente 02 <utente>
2024-11-27 12:02:17 +0100

    fix db conn

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import asyncio
 from db import user_collection, plant_collection, machinery_collection
 
-async def test_connection():
-    user = await user_collection.find_one({})
+def test_connection():
+    user = user_collection.find_one({})
     print(user)
 
-asyncio.run(test_connection())
+test_connection()
 
 #THIS IS JUST A TEST TO SEE IF YOU CAN CONNECT TO THE SERVER, YOU STILL NEED TO GET AN AUTH URI FROM MONGODB CLUSTER
